5f19a63e1f4a9c36f955545a18b8aed6121160e4
Michael LINHARDT <nestoyeur@gmail.com>
2016-05-29 01:58:32 +0200

    project empty

--- srcs/ft_fdf.c
+++ srcs/ft_fdf.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/04 01:49:54 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/05/29 01:54:12 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/05/29 01:57:57 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,27 @@ int		main(void)
 {
 	return (0);
 }
+
+
+
+// -> exemple
+// void     do_key_press(MLXWindow *win, data_t *data, unsigned key)
+// {
+//     if (key == MLX_KEY_ESC)
+//         mlx_loop_stop(data->mlx_ptr);
+// }
+//
+// int           main(void)
+// {
+//     data_t    data;
+//
+//     if ((data.mlx_ptr = mlx_init()) == NULL)
+//         return (EXIT_FAILURE);
+//     if ((data.mlx_win = mlx_window_create(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
+//         return (EXIT_FAILURE);
+//     mlx_window_hook_event(data.mlx_win, MLX_HOOK_KEY_PRESS, do_key_press, &data);
+//     mlx_loop_start(data.mlx_ptr);
+//     mlx_window_destroy(data.mlx_ptr, data.mlx_win);
+//     mlx_deinit(data.mlx_ptr);
+//     return (EXIT_SUCCESS);
+// }
