ddc204baa4f73452ac7f0455626089e44d65b3b1
Michael LINHARDT <nestoyeur@gmail.com>
2016-06-12 07:39:20 +0200

    backup before going to swimingpool <3

--- srcs/ft_fdf.c
+++ srcs/ft_fdf.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/04 01:49:54 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/06/11 09:57:29 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/06/12 06:10:11 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ t_data	*data(void)
 void	data_init(t_data *d, int argc, char **argv)
 {
 	ft_bzero(d, sizeof(t_data));
-	d->scene = INTRO_LOAD;
+	d->scene = MAIN_LOAD;
 	d->scenelast = d->scene;
 	d->img.img = (void *)NULL;
 	d->img.str = (char *)NULL;
--- srcs/ft_fdf_parse.c
+++ srcs/ft_fdf_parse.c
@@ -6,38 +6,11 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/11 02:26:18 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/06/11 10:13:40 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/06/12 07:39:04 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fdf.h"
-
-// int				get_next_line(int fd, char **line)
-// {
-// 	t_gnl		*g;
-//
-// 	if (fd == -10)
-// 		return (gnl_free());
-// 	if (fd < 0 || !(line) || BUFF_SIZE < 1 || !(g = gnl_choose(fd, NULL)) ||
-// 		read(fd, g->b, 0) < 0)
-// 		return (-1);
-// 	while (!(ft_strchr(g->s, '\n')) && (g->r = read(fd, g->b, BUFF_SIZE)) > 0)
-// 	{
-// 		g->t = g->s;
-// 		if (!(g->b[g->r] = '\0'))
-// 			g->s = ft_strjoin(g->t, g->b);
-// 		ft_strdel(&g->t);
-// 	}
-// 	while (g->s[g->i] && g->s[g->i] != '\n' && g->s[g->i] != '\0')
-// 		g->i++;
-// 	if (g->r == 0 && g->i == 0)
-// 		return (gnl_delete(g));
-// 	*line = ft_strsub(g->s, 0, (g->i));
-// 	g->t = g->s;
-// 	g->s = (g->t[g->i] == '\0') ? ft_strnew(0) : ft_strsub(g->t,
-// 	(g->i + 1), (ft_strlen(g->t) - g->i));
-// 	return (1);
-// }
 
 int		pclear(int err, t_data *d, t_map *m, char *msg)
 {
@@ -60,9 +33,8 @@ int		pformat(t_data *d, t_map *m, char c)
 		{
 			// if (!ft_isdigit(c[m->i]) && c[m->i] != ' ' && c[m->i] != '-'
 			// && c[m->i] != '\n' && c[m->i] != '\0')
-			// || (c[m->i] == ' ' && c[m->i + 1])
-			// 		 && (c[m->i + 1] == ' ' || c[m->i + 1] == '\n' || c[m->i + 1] == '\0' ||
-			// return(pclear(1, d, m, "map format error"));
+			// || (c[m->i] == ' ' && c[m->i + 1]) && (c[m->i + 1] == ' ' || c[m->i + 1] == '\n' || c[m->i + 1] == '\0')
+				return(pclear(1, d, m, "map format error"));
 		}
 	else
 		pclear(0, d, m, "read file complet");
@@ -71,6 +43,5 @@ int		pformat(t_data *d, t_map *m, char c)
 
 void	parse(t_data *d, t_map *m)
 {
-	ft_printf("%d %d %d\n", m->i, m->xm, m->ym);
 	(m->status == 1) ? pformat(d, m, (char)NULL) : 0;
 }
