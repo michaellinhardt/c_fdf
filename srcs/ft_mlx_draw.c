4b469cad40598c1fba29ef9c9ebbe3362cf5f390
Michael LINHARDT <nestoyeur@gmail.com>
2016-07-10 08:38:53 +0200

    wip drawing function

--- srcs/ft_mlx_draw.c
+++ srcs/ft_mlx_draw.c
@@ -6,7 +6,7 @@
 /*   By: mlinhard <mlinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/10 03:06:06 by mlinhard          #+#    #+#             */
-/*   Updated: 2016/07/10 08:02:25 by mlinhard         ###   ########.fr       */
+/*   Updated: 2016/07/10 08:38:48 by mlinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,23 @@ void	draw(t_data *d, t_map *m, t_img *i)
 		{
 			ft_printf("Coord: %d,%d\n", m->x, m->y);
 			(m->map[m->y][m->x] == 0) ? (z = 0) : 1;
-			(m->map[m->y][m->x] < 0) ? (z = m->size * 2) : 1;
-			(m->map[m->y][m->x] > 0) ? (z = m->size * -2) : 1;
+			(m->map[m->y][m->x] < 0) ? (z = m->size * 1) : 1;
+			(m->map[m->y][m->x] > 0) ? (z = m->size * -1) : 1;
 			if ((m->x + 1) < m->xm)
 			{
 				(m->map[m->y][m->x + 1] == 0) ? (z2 = 0) : 1;
-				(m->map[m->y][m->x + 1] < 0) ? (z2 = m->size * 2) : 1;
-				(m->map[m->y][m->x + 1] > 0) ? (z2 = m->size * -2) : 1;
-				line(m->x * m->size + (DRAW_X1 + m->size * 2), z + m->y * m->size + (DRAW_Y1 + m->size * 2),
-				(m->x + 1) * m->size + (DRAW_X1 + m->size * 2), z2 + m->y * m->size + (DRAW_Y1 + m->size * 2));
+				(m->map[m->y][m->x + 1] < 0) ? (z2 = m->size * 1) : 1;
+				(m->map[m->y][m->x + 1] > 0) ? (z2 = m->size * -1) : 1;
+				line(m->x * m->size + (DRAW_X1 + m->size * 1), z + m->y * m->size + (DRAW_Y1 + m->size * 1),
+				(m->x + 1) * m->size + (DRAW_X1 + m->size * 1), z2 + m->y * m->size + (DRAW_Y1 + m->size * 1));
 			}
 			if ((m->y + 1) < m->ym)
 			{
 				(m->map[m->y + 1][m->x] == 0) ? (z2 = 0) : 1;
-				(m->map[m->y + 1][m->x] < 0) ? (z2 = m->size * 2) : 1;
-				(m->map[m->y + 1][m->x] > 0) ? (z2 = m->size * -2) : 1;
-				line(m->x * m->size + (DRAW_X1 + m->size * 2), z + m->y * m->size + (DRAW_Y1 + m->size * 2),
-				m->x * m->size + (DRAW_X1 + m->size * 2), z2+ (m->y + 1) * m->size + (DRAW_Y1 + m->size * 2));
+				(m->map[m->y + 1][m->x] < 0) ? (z2 = m->size * 1) : 1;
+				(m->map[m->y + 1][m->x] > 0) ? (z2 = m->size * -1) : 1;
+				line(m->x * m->size + (DRAW_X1 + m->size * 1), z + m->y * m->size + (DRAW_Y1 + m->size * 1),
+				m->x * m->size + (DRAW_X1 + m->size * 1), z2+ (m->y + 1) * m->size + (DRAW_Y1 + m->size * 1));
 			}
 		}
 	}
